4c11ce12dd6051c41851ce4e573ee2379a569ebb
Malinda Kapuruge <malinda.kapuruge@versent.com.au>
2020-04-10 12:44:08 +1000

    more styling for plotly panes

--- src/components/graph.js
+++ src/components/graph.js
@@ -2,16 +2,18 @@ import React from 'react';
 import Plot from 'react-plotly.js';
 
 const Graph = ({graphData, title}) => {
+
 return ( 
       <Plot
         data={graphData}
-        config = {{responsive: true}}
-     
+        config = {{responsive: true, displayModeBar: false}}
         layout={ {
-          font:{size:8}, 
+          font:{size:6}, 
+          width: 420,
+          height: 300,
           xaxis: {showgrid:false},
           autosize:true,
-          title,
+
           legend: {x: 1, y: 1}
         } }
       />  
