32252c997d0467a84273983513f9e1a1c7477a7a
Malinda Kapuruge <malinda.kapuruge@versent.com.au>
2020-04-11 19:59:17 +1000

    show latest stats against a region

--- src/components/state.js
+++ src/components/state.js
@@ -5,46 +5,68 @@ class State extends Component {
 
   constructor(props) {
     super(props);
+    console.log('constructor...');
     this.state = {
-      selectedState: 'Tasmania',
+      selectedState: '',
+      stateLatest: {
+        cases: 0,
+        deaths: 0,
+        recovered: 0,
+      }
     }; 
     this.handleChange = this.handleChange.bind(this);
+    this.setCurrentState = this.setCurrentState.bind(this);
     this.componentDidMount = this.componentDidMount.bind(this);
   }
 
   componentDidMount() {
-    if (this.props.regions.length > 0 ) {
-      this.setState({selectedState:this.props.regions[0].state});
-    }
+    const state = this.props.regions.length > 0 ? this.props.regions[0].state: 'bbb';
+    this.setCurrentState(state);
+  }
+
+  setCurrentState(state) {
+    this.setState({selectedState: state});
+    this.props.regions
+    .filter(r =>  r.state === state)
+    .map(region => {
+      const cases = region.confirmed.length > 0 ? region.confirmed.slice(-1)[0] : 0;
+      const deaths = region.deaths.length > 0 ? region.deaths.slice(-1)[0] : 0;
+      const recovered = region.recovered.length > 0 ? region.recovered.slice(-1)[0] : 0;
+      const latest = {
+        cases, deaths, recovered
+      }
+      this.setState({stateLatest: latest});
+    })
   }
 
   handleChange(e){
-    this.setState({selectedState: e.target.value});
+    this.setCurrentState(e.target.value);
   }
 
   render() {
+    console.log('render...');
     return (
       <Container>
-        <Container className="stateInfo">
-          <Form.Control as="select" value={this.state.selectedState} onChange={this.handleChange}>
-              { this.props.regions.map((r, i) => {
-                  return  <option key={i} value={r.state} >{r.state}</option>
-              })}
-          </Form.Control>
-          <Container className="stateDetails">
-              <span className="dot bginfo">12345</span>
-              <span className="dot bgdanger">34</span>
-              <span className="dot bgsuccess">345</span>
+          <Container className="stateInfo">
+            <Form.Control as="select" value={this.state.selectedState} onChange={this.handleChange}>
+                { this.props.regions.map((r, i) => {
+                    return  <option key={i} value={r.state} >{r.state}</option>
+                })}
+            </Form.Control>
+            <Container className="stateDetails">
+              <span className="dot bginfo">{this.state.stateLatest.cases}</span>
+                <span className="dot bgdanger">{this.state.stateLatest.deaths}</span>
+                <span className="dot bgsuccess">{this.state.stateLatest.recovered}</span>
+            </Container>
+          </Container>
+          <Container>
+            {this.props.regions
+              .filter(r =>  r.state === this.state.selectedState)
+              .map((region, i) => {
+              return <StateGraph key={i} region={region}/>;
+            })}
           </Container>
         </Container>
-        <Container>
-          {this.props.regions
-            .filter(r =>  r.state === this.state.selectedState)
-            .map((region, i) => {
-            return <StateGraph key={i} region={region}/>;
-          })}
-        </Container>
-      </Container>
     );
   }
 }
